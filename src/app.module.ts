a1ff47cf87a6146d67aee65c00cefa2cca532dbf
Eun Oh <bdohhhhh@gmail.com>
2024-11-16 15:43:09 +0900

    [add]: middleware study

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,10 @@
-import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ServeStaticModule } from '@nestjs/serve-static';
@@ -15,6 +21,7 @@ import {
 } from './common/const/env-keys.const';
 import { PUBLIC_FOLDER_PATH } from './common/const/path.const';
 import { ImageModel } from './common/entities/image.entity';
+import { LogMiddleware } from './common/middleware/log.middleware';
 import { PostsModel } from './posts/entities/posts.entity';
 import { PostsModule } from './posts/posts.module';
 import { UsersModel } from './users/entities/users.entity';
@@ -61,4 +68,12 @@ import { UsersModule } from './users/users.module';
     },
   ],
 })
-export class AppModule {}
+// middleware 적용법
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LogMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
