ec430ec148cf46685f0aa3259fe99200b494aaa8
Eun Oh <bdohhhhh@gmail.com>
2024-11-17 13:32:17 +0900

    [update]: comment 기능

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  DefaultValuePipe,
+  Delete,
+  Get,
+  Param,
+  ParseBoolPipe,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+  UseInterceptors,
+} from '@nestjs/common';
+import { QueryRunner } from 'src/common/decorator/query-runner.decorator';
+import { TransactionInterceptor } from 'src/common/interceptor/transaction.interceptor';
+import { QueryRunner as QR } from 'typeorm';
 import { RolesEnum } from './const/roles.const';
 import { Roles } from './decorators/roles.decorator';
+import { User } from './decorators/user.decorator';
+import { UsersModel } from './entity/users.entity';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -9,17 +26,75 @@ export class UsersController {
 
   @Get()
   @Roles(RolesEnum.ADMIN)
-  // @UseInterceptors(ClassSerializerInterceptor)
+  /**
+   * serialization -> 직렬화 -> 현재 시스템에서 사용되는 (NestJS) 데이터의 구조를 다른 시스템에서도 쉽게
+   *                          사용 할 수 있는 포맷으로 변환
+   *                        -> class의 object에서 JSON 포맷으로 변환
+   * deserialization -> 역직렬화
+   */
   getUsers() {
     return this.usersService.getAllUsers();
   }
 
+  @Get('follow/me')
+  async getFollow(
+    @User() user: UsersModel,
+    @Query('includeNotConfirmed', new DefaultValuePipe(false), ParseBoolPipe)
+    includeNotConfirmed: boolean,
+  ) {
+    return this.usersService.getFollowers(user.id, includeNotConfirmed);
+  }
+
+  @Post('follow/:id')
+  async postFollow(
+    @User() user: UsersModel,
+    @Param('id', ParseIntPipe) followeeId: number,
+  ) {
+    await this.usersService.followUser(user.id, followeeId);
+
+    return true;
+  }
+
+  @Patch('follow/:id/confirm')
+  @UseInterceptors(TransactionInterceptor)
+  async patchFollowConfirm(
+    @User() user: UsersModel,
+    @Param('id', ParseIntPipe) followerId: number,
+    @QueryRunner() qr: QR,
+  ) {
+    await this.usersService.confirmFollow(followerId, user.id, qr);
+
+    await this.usersService.incrementFollowerCount(user.id, qr);
+
+    await this.usersService.incrementFolloweeCount(followerId, qr);
+
+    return true;
+  }
+
+  @Delete('follow/:id')
+  @UseInterceptors(TransactionInterceptor)
+  async deleteFollow(
+    @User() user: UsersModel,
+    @Param('id', ParseIntPipe) followeeId: number,
+    @QueryRunner() qr: QR,
+  ) {
+    await this.usersService.deleteFollow(user.id, followeeId, qr);
+
+    await this.usersService.decrementFollowerCount(followeeId, qr);
+
+    await this.usersService.decrementFolloweeCount(user.id, qr);
+
+    return true;
+  }
+
   // @Post()
-  // postUser(
-  //   @Body('nickname') nickname: string,
+  // postUser(@Body('nickname') nickname: string,
   //   @Body('email') email: string,
-  //   @Body('password') password: string,
-  // ) {
-  //   return this.usersService.createUser({ nickname, email, password });
+  //   @Body('password') password: string) {
+  //     return this.usersService.createUser({
+  //       nickname,
+  //       email,
+  //       password,
+  //     });
   // }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { QueryRunner, Repository } from 'typeorm';
+import { UserFollowersModel } from './entity/user-followers.entity';
 import { UsersModel } from './entity/users.entity';
 
 @Injectable()
@@ -8,33 +9,46 @@ export class UsersService {
   constructor(
     @InjectRepository(UsersModel)
     private readonly usersRepository: Repository<UsersModel>,
+    @InjectRepository(UserFollowersModel)
+    private readonly userFollowersRepository: Repository<UserFollowersModel>,
   ) {}
 
-  async getAllUsers() {
-    return this.usersRepository.find();
+  getUsersRepository(qr?: QueryRunner) {
+    return qr
+      ? qr.manager.getRepository<UsersModel>(UsersModel)
+      : this.usersRepository;
   }
 
-  async createUser(user: Pick<UsersModel, 'nickname' | 'email' | 'password'>) {
-    // 닉네임 중복이 없는지 확인
-    // exist
+  getUserFollowRepository(qr?: QueryRunner) {
+    return qr
+      ? qr.manager.getRepository<UserFollowersModel>(UserFollowersModel)
+      : this.userFollowersRepository;
+  }
+
+  async createUser(user: Pick<UsersModel, 'email' | 'nickname' | 'password'>) {
+    // 1) nickname 중복이 없는지 확인
+    // exist() -> 만약에 조건에 해당되는 값이 있으면 true 반환
     const nicknameExists = await this.usersRepository.exists({
       where: {
         nickname: user.nickname,
       },
     });
 
-    if (nicknameExists)
-      throw new BadRequestException('이미 존재하는 nickname 입니다');
+    if (nicknameExists) {
+      throw new BadRequestException('이미 존재하는 nickname 입니다!');
+    }
 
-    const emailExist = await this.usersRepository.exists({
+    const emailExists = await this.usersRepository.exists({
       where: {
         email: user.email,
       },
     });
 
-    if (emailExist) throw new BadRequestException('이미 가입한 이메일 입니다.');
+    if (emailExists) {
+      throw new BadRequestException('이미 가입한 이메일입니다!');
+    }
 
-    const userObject = await this.usersRepository.create({
+    const userObject = this.usersRepository.create({
       nickname: user.nickname,
       email: user.email,
       password: user.password,
@@ -45,11 +59,166 @@ export class UsersService {
     return newUser;
   }
 
+  async getAllUsers() {
+    return this.usersRepository.find();
+  }
+
   async getUserByEmail(email: string) {
     return this.usersRepository.findOne({
       where: {
         email,
       },
     });
+  }
+
+  async followUser(followerId: number, followeeId: number, qr?: QueryRunner) {
+    const userFollowersRepository = this.getUserFollowRepository(qr);
+
+    await userFollowersRepository.save({
+      follower: {
+        id: followerId,
+      },
+      followee: {
+        id: followeeId,
+      },
+    });
+
+    return true;
+  }
+
+  async getFollowers(userId: number, includeNotConfirmed: boolean) {
+    /**
+     * [
+     *  {
+     *      id: number;
+     *      follower: UsersModel;
+     *      followee: UsersModel;
+     *      isConfirmed: boolean;
+     *      createdAt: Date;
+     *      updatedAt: Date;
+     *  }
+     * ]
+     */
+    const where = {
+      followee: {
+        id: userId,
+      },
+    };
+
+    if (!includeNotConfirmed) {
+      where['isConfirmed'] = true;
+    }
+
+    const result = await this.userFollowersRepository.find({
+      where,
+      relations: {
+        follower: true,
+        followee: true,
+      },
+    });
+
+    return result.map((x) => ({
+      id: x.follower.id,
+      nickname: x.follower.nickname,
+      email: x.follower.email,
+      isConfirmed: x.isConfirmed,
+    }));
+  }
+
+  async confirmFollow(
+    followerId: number,
+    followeeId: number,
+    qr?: QueryRunner,
+  ) {
+    const userFollowersRepository = this.getUserFollowRepository(qr);
+
+    const existing = await userFollowersRepository.findOne({
+      where: {
+        follower: {
+          id: followerId,
+        },
+        followee: {
+          id: followeeId,
+        },
+      },
+      relations: {
+        follower: true,
+        followee: true,
+      },
+    });
+
+    if (!existing) {
+      throw new BadRequestException('존재하지 않는 팔로우 요청입니다.');
+    }
+
+    await userFollowersRepository.save({
+      ...existing,
+      isConfirmed: true,
+    });
+
+    return true;
+  }
+
+  async deleteFollow(followerId: number, followeeId: number, qr?: QueryRunner) {
+    const userFollowersRepository = this.getUserFollowRepository(qr);
+
+    await userFollowersRepository.delete({
+      follower: {
+        id: followerId,
+      },
+      followee: {
+        id: followeeId,
+      },
+    });
+
+    return true;
+  }
+
+  async incrementFollowerCount(userId: number, qr?: QueryRunner) {
+    const userRepository = this.getUsersRepository(qr);
+
+    await userRepository.increment(
+      {
+        id: userId,
+      },
+      'followerCount',
+      1,
+    );
+  }
+
+  async decrementFollowerCount(userId: number, qr?: QueryRunner) {
+    const userRepository = this.getUsersRepository(qr);
+
+    await userRepository.decrement(
+      {
+        id: userId,
+      },
+      'followerCount',
+      1,
+    );
+  }
+
+  async incrementFolloweeCount(userId: number, qr?: QueryRunner) {
+    const userRepository = this.getUsersRepository(qr);
+
+    await userRepository.increment(
+      {
+        id: userId,
+      },
+      'followeeCount',
+      1,
+    );
+  }
+
+  async decrementFolloweeCount(userId: number, qr?: QueryRunner) {
+    const userRepository = this.getUsersRepository(qr);
+
+    await userRepository.decrement(
+      {
+        id: userId,
+      },
+      'followeeCount',
+      1,
+    );
   }
 }
